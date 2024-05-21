a308207d1d5b5ca24470bbcb2e5d4c5ca87d1346
Chiari13 <147656879+Chiari13@users.noreply.github.com>
2024-05-21 18:44:00 -0300

    Update Entidad.java
    Agregué constructores vacíos.

--- src/Modelos/Entidades/Entidad.java
+++ src/Modelos/Entidades/Entidad.java
@@ -4,7 +4,12 @@ public abstract class Entidad {
     private String nombre;
     private int salud;
 
-    //constructor
+    //Constructor
+    public Entidad () {
+        this.nombre = "";
+        this.salud = 0;
+    }
+    
     public Entidad(String nombre, int salud) {
         this.nombre = nombre;
         this.salud = salud;
@@ -28,6 +33,8 @@ public abstract class Entidad {
     public void setSalud(int salud) {
         this.salud = salud;
     }
+
+    //Metodos
 
     abstract boolean estaVivo();
     abstract void recibirDanio(int danio);
